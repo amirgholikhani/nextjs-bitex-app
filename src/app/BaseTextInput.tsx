58076d4788be621fde393cf14d32575d40ec9611
Amir Gholikhani <amirgholikhani70@gmail.com>
2023-11-09 09:34:18 +0330

    Add BaseTextInput component

---
+++ src/app/BaseTextInput.tsx
@@ -0,0 +1,9 @@
+function BaseTextInput() {
+    return (
+        <div className="mt-10">
+            <input className="rounded-lg text-[#434343] w-[200px] h-[36px] px-2 focus:outline-none" placeholder="Add tags ..." />
+        </div>
+    )
+}
+
+export default BaseTextInput
